f921f2e7fcef4e1d8bd3862b4803617ce1adee7f
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-09 14:39:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,12 @@
 def speak_to_grandma (phrase)
   if phrase == phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
 end
 end
